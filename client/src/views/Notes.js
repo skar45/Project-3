194578834fbe52b4e217d58686f89efd4280a4ff
Shihan Zhang <corazhang0@gmail.com>
2020-11-26 14:42:48 -0500

    This version is not working properly

--- client/src/views/Notes.js
+++ client/src/views/Notes.js
@@ -1,31 +1,35 @@
-import React from "react";
-import Card from "../components/Card"
-
-function Notes(props) {
+import React, { Component } from "react";
+//import noteReducer from ".. /reducers/noteReducer";
+class Note extends Component {
+  render() {
     return (
-      <div>
-          <div className="row">
-            <div className="col-sm-6">
-              <Card />
-            </div>
-            <div className="col-sm-6">
-              <Card /> 
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-6">
-              <Card /> 
-            </div>
-            <div className="col-sm-6">
-              <Card /> 
-            </div>
-          </div>
+      <div className="note">
+        <h2 className="note_title">{this.props.note.title}</h2>
+        <p className="note_message">{this.props.note.message}</p>
+        <div className="control-buttons">
+          <button
+            className="edit"
+            onClick={() =>
+              this.props.dispatch({ type: "EDIT_NOTE", id: this.props.note.id })
+            }
+          >
+            Edit
+          </button>
+
+          <button
+            className="delete"
+            onClick={() =>
+              this.props.dispatch({
+                type: "DELETE_NOTE",
+                id: this.props.note.id,
+              })
+            }
+          >
+            Delete
+          </button>
         </div>
-      
-      
+      </div>
     );
   }
-  
-  export default Notes;
-
-  
+}
+export default Note;
