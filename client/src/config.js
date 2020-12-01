f579adae629e72b76fe1885c063f556cc279d356
Sajal <karkeesajal@gmail.com>
2020-11-30 20:53:02 -0500

    bug fix

--- client/src/config.js
+++ client/src/config.js
@@ -4,12 +4,12 @@ export default {
   oidc: {
     clientId: process.env.Client_Id || '0oa1d3kgklSfhS18Z5d6',
     issuer: process.env.Issuer || 'https://dev-7705867.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:8080/login/callback',
+    redirectUri: 'https://sleepy-retreat-90049.herokuapp.com/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,
   },
   resourceServer: {
-    messagesUrl: 'http://localhost:8080/api/messages',
+    messagesUrl: 'https://sleepy-retreat-90049.herokuapp.com/api/messages',
   },
 };
