fb3dad2c0b6f693c9ad515221cd79ce5cb8a1098
Dailey Kaze <daileykaze@gmail.com>
2020-12-01 10:29:40 -0500

    fixed

--- client/src/components/AllEvents.js
+++ client/src/components/AllEvents.js
@@ -84,7 +84,7 @@ export default function AllEvents(props){
                       <h5 className="card-title">{event.title}</h5>
                       <p className="card-text"></p>
                       <a href="/CalendarMain" className="btn btn-primary">View in Calendar</a>
-                      <a href="#" className="btn btn-primary" onClick={()=>options(event)}>Set Reminder</a>
+                      <a href="#" className="btn btn-primary" onClick={()=>sendMail(event)}>Set Reminder</a>
 
                     </div>
                     <div className="card-footer text-muted">
